cf05763340b678c3fe13cc18b7240b8f372cda3e
esteng <elias.stengel@gmail.com>
2016-12-11 15:28:49 -0500

    removing offenders

--- french/french_test.py
+++
@@ -1,6 +0,0 @@
-from infag_parser import parse_file
-
-all_words = parse_file("16Nov29-191756-D150-P10-S10-B150-O1-t64-k0.75-Gfrench.grammar/infag-2")
-with open('french_test',"w") as f1:
-    for k,v in all_words.items():
-        f1.write("{}:{}\n".format(k,v[0]))
